c2ea9a056e6d95d317ea80ff4e5e3cdfcae7ef7b
nriser <riserika@gmail.com>
2017-07-27 11:29:13 -0400

    Add input field that edits hero name
    Implement two-way binding between the input form element and the
    hero.name property using ngModel directive in the component HTML
    template.
    
    To do so, import FormsModule in app.module.ts, since NgModel lives
    inside FormsModule.
    
    Users are now able to edit the hero name in an <input> textbox.
    The textbox displays the hero's name property and update that property
    as the user types.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,17 @@ export class Hero {
 
 @Component({
   selector: 'my-app',
-  // interpolation binding syintax
+  // Two-way data binding: [(ngModel)] directive is the Angular syntax to bind the hero.name property to the textbox. Data flows in both directions: from the property to the textbox, and from the textbox back to the property.
+  // Although NgModel is a valid Angular directive, it isn't available by default. It belongs to the optional FormsModule. You must opt-in to using that module. To do so, import FormsModule in app.module.ts.
   template: `
-  <h1>{{title}}</h1>
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
-  <div><label>name: </label>{{hero.name}}</div>
-  `,
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `
 })
 export class AppComponent {
   title = 'Tour of Heroes';
@@ -24,3 +28,5 @@ export class AppComponent {
     name: 'Windstorm'
   };
 }
+
+// Users are now able to edit the hero name in an <input> textbox. The textbox displays the hero's name property and update that property as the user types.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule } from '@angular/forms'; // <-- NgModel lives here!
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:[ BrowserModule,
+            FormsModule // <-- import the FormsModule before binding using [(ngModel)] in app.component.ts
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
