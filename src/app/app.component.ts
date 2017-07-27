fe474da39160ee5575b67276d912300377bed70d
nriser <riserika@gmail.com>
2017-07-27 13:57:35 -0400

    Display all heroes
    Create a HEROES array (which is an array based on the Hero model),
    and add a property inside AppComponent class that references the heroes
    array.
    
    In the HTML template, use the ngFor directive to loop through array of
    heroes, HEROES, to display all heroes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,44 @@ export class Hero {
   name: string;
 }
 
+// a HEROES array of type Hero. An array based on the Hero class above.
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'my-app',
   // Two-way data binding: [(ngModel)] directive is the Angular syntax to bind the hero.name property to the textbox. Data flows in both directions: from the property to the textbox, and from the textbox back to the property.
   // Although NgModel is a valid Angular directive, it isn't available by default. It belongs to the optional FormsModule. You must opt-in to using that module. To do so, import FormsModule in app.module.ts.
   template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
+    <h2>My Heroes</h2>
+      <ul class="heroes">
+        <li *ngFor="let hero of heroes">
+          <span class="badge">{{hero.id}}</span> {{hero.name}}
+        </li>
+      </ul>
     `
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  // Convert the hero from a literal string to a class. Set the component's hero property to be of type Hero, then initialize it with an id of 1 and the name Windstorm.
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // a public property in AppComponent that exposes the heroes for binding.
+  heroes = HEROES; // The heroes type isn't defined because TypeScript infers it from the HEROES array.
 }
 
-// Users are now able to edit the hero name in an <input> textbox. The textbox displays the hero's name property and update that property as the user types.
+
+
+
+// The ngFor directive iterates over the component's heroes array and renders an instance of this template for each hero in that array.
+// The let hero part of the expression identifies hero as the template input variable (create a variable), which holds the CURRENT hero item for each iteration. You can reference this variable within the template to access the current hero's properties.
+
+// The ngFor directive iterates over the component's heroes array and renders an instance of this template for each hero in that array.
