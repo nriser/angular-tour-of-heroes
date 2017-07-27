3e4f70831263cf4820283a0f6c1359b8b22796e4
nriser <riserika@gmail.com>
2017-07-27 11:11:51 -0400

    Display hero details
    - Create Hero class
    - Access Hero class from AppComponent
    - Display Hero properties in the view via the HTML template, inside
    AppComponent decorator using interpolation binding syntax
    (one-way binding from component to HTML).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
 
+// Hero model
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  // interpolation binding syintax
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div><label>name: </label>{{hero.name}}</div>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  title = 'Tour of Heroes';
+  // Convert the hero from a literal string to a class. Set the component's hero property to be of type Hero, then initialize it with an id of 1 and the name Windstorm.
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
